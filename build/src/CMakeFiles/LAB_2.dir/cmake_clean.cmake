037eeec604473dc76a346fe73b9f6269b121e7c1
WwWwIvanwWwW <endalovivan@gmail.com>
2026-04-23 14:49:42 +0300

    Struct project

---
+++ build/src/CMakeFiles/LAB_2.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "../LAB_2"
+  "../LAB_2.pdb"
+  "CMakeFiles/LAB_2.dir/main.cpp.o"
+  "CMakeFiles/LAB_2.dir/main.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/LAB_2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/main.cpp
@@ -0,0 +1,5 @@
+#include <iostream>
+int main(){
+    std::cout << "САСАТЬ АМЕРИКА!!!" << std::endl;
+    return 0;
+}
